421c100a3343457668229f88e2deb5d56565b502
scrawley5 <samgcrawley@gmail.com>
2020-06-23 15:34:31 +0100

    Commit with functions which create and use inverted matrices with caching ability

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create and use inverted matrices with caching ability
 
-## Write a short comment describing this function
+## Creates a cacheable matrix to input into cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(!is.matrix(x)){
+    print("Please input a matrix.")
+  }
+  
+  inverse.matrix<-NULL
+  
+  set<-function(y){
+    x<<-y
+    inverse.matrix<<-NULL
+  }
+  
+  get<-function()x
+  
+  set.inverse<-function(solve) inverse.matrix<<-solve
+  get.inverse<-function() inverse.matrix
+  
+  list(set=set,
+       get=get,
+       set.inverse=set.inverse,
+       get.inverse=get.inverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse of cacheable matrix which is returned by makeCacheMatrix()
+## If inverse has already been calculated and the matrix is unchanged, cacheSolve() returns the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse.matrix<-x$get.inverse
+  
+  if(!is.null(inverse.matrix)){
+    message("Gettng cached inverse matrix")
+    return(inverse.matrix)
+  }
+  
+  matrix.to.inverse<-x$get()
+  inverse.matrix<-solve(matrix.to.inverse)
+  x$set.inverse(inverse.matrix)
+  inverse.matrix
+  
 }
